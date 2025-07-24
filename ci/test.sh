d111a9514c9d41eeda18894183e13e5f05f1a37a
llavski <kieaki@gmail.com>
2025-07-25 00:16:57 +0200

    Initialize gh-pages

--- ci/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-export GO111MODULE="on"
-go test -tags 's3 metadata' -v -race -coverprofile=coverage.txt -covermode=atomic -coverpkg github.com/llavskii/selenoid,github.com/llavskii/selenoid/session,github.com/llavskii/selenoid/config,github.com/llavskii/selenoid/protect,github.com/llavskii/selenoid/service,github.com/llavskii/selenoid/upload,github.com/llavskii/selenoid/info,github.com/llavskii/selenoid/jsonerror
-
-go install golang.org/x/vuln/cmd/govulncheck@latest
-"$(go env GOPATH)"/bin/govulncheck -tags production ./...
--- doc.go
+++
@@ -1,4 +0,0 @@
-/*
-Selenoid is a powerful implementation of Selenium Hub using Docker or standalone web driver binaries to start and launch browsers. Documentation has moved to: https://aerokube.com/selenoid/latest.
-*/
-package main
